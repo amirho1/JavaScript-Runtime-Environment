c161eb781bf5046ef705468f9eb5af98c0750255
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-01-14 16:31:43 +0330

    feat(Queue): add queue data type

---
+++ src/Queue.ts
@@ -0,0 +1,59 @@
+/**
+ * A class representing a Queue data structure.
+ */
+export default class Queue {
+  /**
+   * The internal array representing the queue.
+   * @private
+   */
+  private queue: any[] = [];
+
+  /**
+   * Creates an instance of Queue.
+   * @param args - Initial elements to populate the queue.
+   */
+  constructor(...args: any[]) {
+    this.queue = args;
+    console.log("hello from queue");
+  }
+
+  /**
+   * Adds an item to the end of the queue.
+   * @param item - The item to be added to the queue.
+   */
+  public enqueue(item: any) {
+    this.queue.push(item);
+  }
+
+  /**
+   * Checks if the queue is empty.
+   * @returns A boolean indicating whether the queue is empty.
+   */
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+
+  /**
+   * Returns the item at the front of the queue without removing it.
+   * @returns The item at the front of the queue.
+   */
+  peek() {
+    return this.queue[0];
+  }
+
+  /**
+   * Removes and returns the item at the front of the queue.
+   * @returns The item that was removed from the front of the queue.
+   */
+  dequeue() {
+    return this.queue.shift();
+  }
+
+  /**
+   * Returns the number of items in the queue.
+   * @returns The size of the queue.
+   */
+  size() {
+    return this.queue.length;
+  }
+}

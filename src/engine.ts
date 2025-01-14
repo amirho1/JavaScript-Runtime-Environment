98f13dbd16f13d0f32eca774241cb25829c38f6e
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-01-14 16:33:30 +0330

    feat(javascriptEngine): rename to engine

---
+++ src/engine.ts
@@ -0,0 +1,7 @@
+import Stack from "./Stack";
+
+export class Engine {
+  constructor() {
+    console.log('Engine started')
+  }
+}
--- src/javascripEngine.ts
+++
@@ -1,7 +0,0 @@
-import Stack from "./Stack";
-
-export function JavascriptRunTime() {
-  const stack = new Stack();
-
-  return stack;
-}

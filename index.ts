4141312846926878778fdad67cda36e7574bceb3
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-01-17 15:20:12 +0330

    feat(runtime): integrate editor with runtime and add run button functionality

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-import "./src/editor";
+import editor from "./src/editor";
+import Runtime from "./src/runtime";
 
+const runtime = new Runtime(editor);
 
+(function IIFE() {
+  editor.focus();
+
+  const runButton = document.getElementById("run");
+
+  runButton?.addEventListener("click", () => {
+    runtime.run();
+  });
+})();
---
+++ src/types/index.d.ts
@@ -0,0 +1,5 @@
+
+
+export interface Console {
+  log: (...args: any[]) => void;
+}

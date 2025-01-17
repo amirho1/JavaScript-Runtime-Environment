0a903cee6ea2f13f8d19165994deae9379907c0b
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-01-17 15:06:49 +0330

    update(editor): export initialized editor remove unused functions

--- src/editor.ts
+++ src/editor.ts
@@ -6,21 +6,12 @@ import debounce from "debounce";
 
 const languageConf = new Compartment();
 
-function handleEditorChange() {
-  console.log(editor.state.doc.toString());
-}
-
-const handleUpdate = EditorView.updateListener.of(debounce(handleEditorChange, 500));
-
 const editor = new EditorView({
-  doc: 'console.log("hello")',
-  extensions: [basicSetup, languageConf.of(javascript()), dracula, handleUpdate],
+  doc: "console.log(1 + 2 + 3)",
+  extensions: [basicSetup, languageConf.of(javascript()), dracula],
   parent: document.querySelector("#editor") as HTMLElement,
 });
 
 editor.focus();
 
-// Function to get the text value from the editor
-function getEditorValue() {
-  return editor.state.doc;
-}
+export default editor;

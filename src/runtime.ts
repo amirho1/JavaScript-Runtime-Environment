61bc6822415476943df0bad73c5d1e664d323588
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-01-17 15:18:06 +0330

    feat(runtime): initial runtime features

---
+++ src/runtime.ts
@@ -0,0 +1,71 @@
+import { EditorView } from "codemirror";
+import { Engine } from "./engine";
+import EventLoop from "./eventLoop";
+import Queue from "./Queue";
+import Stack from "./Stack";
+import { Statement } from "acorn";
+import * as astring from "astring";
+import hljs from "highlight.js";
+import javascript from "highlight.js/lib/languages/javascript";
+import { Console } from "./types";
+
+hljs.registerLanguage("javascript", javascript);
+
+export default class Runtime {
+  private stack: Stack<Statement>;
+  private taskQueue: Queue;
+  private microTaskQueue: Queue;
+  private engine: Engine;
+  private eventLoop: EventLoop;
+  private editor: EditorView;
+  private console: Console = {
+    log(...args: any[]) {
+      const log = document.createElement("div");
+      const consoleElement = document.querySelector(".item-wrapper");
+      const innerHTML = args.reduce((prev, arg) => `${prev} ${arg}`, "");
+      log.innerHTML = innerHTML;
+      consoleElement?.appendChild(log);
+      log.scrollIntoView();
+    },
+  };
+
+  constructor(editor: EditorView) {
+    this.stack = new Stack<Statement>();
+    this.taskQueue = new Queue();
+    this.microTaskQueue = new Queue();
+    this.engine = new Engine(this.stack, this.console);
+    this.eventLoop = new EventLoop();
+    this.editor = editor;
+
+    this.addElementFromCallStackToUI = this.addElementFromCallStackToUI.bind(this);
+    this.removeElementFromCallStackUI = this.removeElementFromCallStackUI.bind(this);
+
+    this.stack.onPush(this.addElementFromCallStackToUI);
+    this.stack.onPop(this.removeElementFromCallStackUI);
+  }
+
+  run() {
+    const stackItemWrapper = document.getElementById("stack-items-wrapper");
+    if (stackItemWrapper) stackItemWrapper.innerHTML = "";
+    this.stack.clear();
+
+    this.engine.run(this.editor.state.doc.toString());
+  }
+
+  addElementFromCallStackToUI() {
+    const element = document.getElementById("stack-items-wrapper");
+    const funcElement = document.createElement("div");
+
+    const code = astring.generate(this.stack.peek());
+    const highlightedCode = hljs.highlight(code, { language: "javascript" }).value;
+
+    funcElement.innerHTML = highlightedCode;
+    funcElement?.classList.add("stack-element");
+
+    element?.appendChild(funcElement);
+  }
+
+  removeElementFromCallStackUI() {
+    return;
+  }
+}

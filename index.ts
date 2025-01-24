479c5bdd1b1ac78c1ba8a6dbe8ea85d8b0911a3a
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-01-24 13:48:48 +0330

    chore(index root): initialize the highlight js in root index

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import editor from "./src/editor";
 import Runtime from "./src/runtime";
+import javascript from "highlight.js/lib/languages/javascript";
+import hljs from "highlight.js";
 
 const runtime = new Runtime(editor);
 
 (function IIFE() {
+  hljs.registerLanguage("javascript", javascript);
   editor.focus();
 
   const runButton = document.getElementById("run");

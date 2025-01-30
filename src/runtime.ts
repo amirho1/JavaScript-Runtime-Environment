d16e962c67638fe5ba22bc13777fa428c9ec0c8b
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-01-30 14:22:47 +0330

    feat(runtime): replace Map with ObservableMap for subscription

--- src/runtime.ts
+++ src/runtime.ts
@@ -6,12 +6,13 @@ import Ui from "./ui";
 import WebAPIs from "./webAPIs";
 import Stack from "./Stack";
 import Queue from "./Queue";
+import ObservableMap from "./ObservableMap";
 
 export default class Runtime {
   private stack = new Stack<Statement>();
   private taskQueue = new Queue<Statement>();
   private microTaskQueue = new Queue<Statement>();
-  private map = new Map();
+  private map = new ObservableMap();
   private ui = new Ui("#stack", this.stack, this.taskQueue, this.microTaskQueue, this.map);
   private eventLoop = new EventLoop(this.ui);
   private webApi = new WebAPIs(this.taskQueue, this.ui, this.map);

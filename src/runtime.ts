b550b2d44f7e74fa3095ad451146ac8bcaa7bb85
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-01-30 20:04:24 +0330

    feat(runtime): refactor Runtime class to improve event loop and engine integration

--- src/runtime.ts
+++ src/runtime.ts
@@ -13,10 +13,11 @@ export default class Runtime {
   private taskQueue = new Queue<Statement>();
   private microTaskQueue = new Queue<Statement>();
   private map = new ObservableMap();
+  private isStackEmpty = { value: true };
   private ui = new Ui("#stack", this.stack, this.taskQueue, this.microTaskQueue, this.map);
-  private eventLoop = new EventLoop(this.ui);
-  private webApi = new WebAPIs(this.taskQueue, this.ui, this.map);
-  private engine = new Engine(this.stack, this.ui, this.webApi);
+  private eventLoop = new EventLoop(this.microTaskQueue, this.taskQueue, this.isStackEmpty);
+  private webApi = new WebAPIs(this.taskQueue, this.map);
+  private engine = new Engine(this.stack, this.ui, this.webApi, this.isStackEmpty);
 
   constructor(private editor: EditorView) {}
 
@@ -29,5 +30,8 @@ export default class Runtime {
     this.microTaskQueue.clear();
     this.engine.run(this.editor.state.doc.toString());
     this.map.clear();
+
+    this.taskQueue.on("enqueue", () => this.eventLoop.run(this.engine));
+    this.microTaskQueue.on("enqueue", () => this.eventLoop.run(this.engine));
   }
 }
